3f4bac67564b68cfead35ad63ff7e5e8a8eb9d75
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-17 16:06:50 -0300

    Add files via upload

---
+++ php/register.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+include("conexao.php");
+
+if(empty($_POST["nome_req"]) || empty($_POST["matricula"]) || empty($_POST["email"]) || empty($_POST["telefone"]) || empty($_POST["assunto"]) || empty($_POST["resolucao"])){
+    $_SESSION["campos_vazios"] = true;
+    header("Location: ../registrar.php");
+    exit();
+}
+
+$responsavel = mysqli_escape_string($conexao,$_SESSION["nome"]);
+$requisitante = mysqli_escape_string($conexao,$_POST["nome_req"]);
+$matricula = mysqli_escape_string($conexao,$_POST["matricula"]);
+$email = mysqli_escape_string($conexao,$_POST["email"]);
+$telefone = mysqli_escape_string($conexao,$_POST["telefone"]);
+$assunto = mysqli_escape_string($conexao,$_POST["assunto"]);
+$resolucao = mysqli_escape_string($conexao,$_POST["resolucao"]);
+
+$query = "INSERT INTO registros (responsavel, requisitante, matricula, email, telefone, assunto, resolucao,data,hora) VALUES ('{$responsavel}', '{$requisitante}', '{$matricula}', '{$email}', '{$telefone}', '{$assunto}', '{$resolucao}',SYSDATE(),CURRENT_TIME())";
+
+if($conexao->query($query) === TRUE){
+    $_SESSION["registrado"] = true;
+}
+header("Location: ../registrar.php");
+exit();
+
+
