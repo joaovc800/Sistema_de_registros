89758882ec47f3e1a52c8bd803c1be2a2451e9af
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-18 19:26:19 -0300

    Update mascara.js

--- js/mascara.js
+++ js/mascara.js
@@ -21,3 +21,22 @@ function mask(o, f) {
     }
     return r;
   }
+
+  function mascaraData( campo, e ){
+	var kC = (document.all) ? event.keyCode : e.keyCode;
+	var data = campo.value;
+	
+	if( kC!=8 && kC!=46 )
+	{
+		if( data.length==2 )
+		{
+			campo.value = data += '/';
+		}
+		else if( data.length==5 )
+		{
+			campo.value = data += '/';
+		}
+		else
+			campo.value = data;
+	}
+}
