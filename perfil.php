b50ab4ef03ebc6235916e02809c408b809648ee3
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-18 21:49:58 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -14,7 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
-	  <link rel="stylesheet" href="css/style2.css">
+    <link rel="stylesheet" href="css/style2.css">
     <style>
         @media screen and (max-width: 600px){
             #sidebar,#sidebarCollapse{
@@ -127,6 +127,10 @@
                 <div class="col-12 mt-4">
                     <label>E-mail</label>
                     <input type="text" class="form-control" readonly value="<?php echo $email?>">
+                </div>
+                <div class="col-12 mt-4">
+                    <label>Sessão Service Desk</label>
+                    <input type="text" class="form-control" readonly value="<?php echo $cargo?>">
                 </div>
                 <div class="col-12 mt-4">
                     <label>Matrícula</label>
