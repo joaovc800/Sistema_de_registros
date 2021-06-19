139af408f5402c31ca97be7b82825ca9b94cb38a
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-19 10:27:43 -0300

    Update registros.php

--- registros.php
+++ registros.php
@@ -49,10 +49,10 @@
             <a href="perfil.php"><span class="fas fa-user"></span> Perfil</a>
           </li>
           <li>
-            <a href="http://os.uninove.br/os/"><span class="fas fa-cogs"></span>O.S</a>
+            <a href="http://os.uninove.br/os/"target="_blank"><span class="fas fa-cogs"></span>O.S</a>
           </li>
           <li>
-            <a href="http://portalsd.uninove.br/"><span class="fas fa-door-open"></span>Portal SD</a>
+            <a href="http://portalsd.uninove.br/"target="_blank"><span class="fas fa-door-open"></span>Portal SD</a>
           </li>
           <li>
             <a href="php/logout.php"><span class="fas fa-sign-out-alt"></span>Sair</a>
@@ -99,10 +99,10 @@
                     <a class="nav-link" href="perfil.php">Perfil</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="http://os.uninove.br/os/">O.S</a>
+                  <a class="nav-link" href="http://os.uninove.br/os/"target="_blank">O.S</a>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="http://portalsd.uninove.br/">Portal SD</a>
+                  <a class="nav-link" href="http://portalsd.uninove.br/"target="_blank">Portal SD</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="php/logout.php">Sair</a>
