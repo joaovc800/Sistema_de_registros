17b571daa80778d8dd6c53c329daf118d570fbaa
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-18 21:25:20 -0300

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -14,6 +14,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
+	  <link rel="stylesheet" href="css/style2.css">
     <style>
         @media screen and (max-width: 600px){
             #sidebar,#sidebarCollapse{
