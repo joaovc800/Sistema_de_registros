01d3c5557b8c1c912efc726bacfbaaac190a8dfb
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-21 20:50:12 -0300

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -178,9 +178,5 @@
 		</div>
     <script src="https://kit.fontawesome.com/5a9643203d.js" crossorigin="anonymous"></script>
     <script src="js/mascara.js"></script>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
   </body>
 </html>
