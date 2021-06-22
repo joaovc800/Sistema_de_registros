ee3bcd49c9a8ea84baac468ad54d1025a6bd5287
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-21 22:44:41 -0300

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -169,5 +169,9 @@
     <script src="js/mascara.js"></script>
     <script src="https://kit.fontawesome.com/5a9643203d.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+< script  src = " js / jquery.min.js " > </ script >
+    < script  src = " js / popper.js " > </ script >
+    < script  src = " js / bootstrap.min.js " > </ script >
+    < script  src = " js / main.js " > </ script >
   </body>
 </html>
