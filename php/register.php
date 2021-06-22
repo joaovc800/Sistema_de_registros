dc2c19eca9c049b8a1ec778fa1ccb74ba3402cdb
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-06-21 21:24:38 -0300

    Update register.php

--- php/register.php
+++ php/register.php
@@ -16,6 +16,8 @@ $telefone = mysqli_escape_string($conexao,$_POST["telefone"]);
 $assunto = mysqli_escape_string($conexao,$_POST["assunto"]);
 $resolucao = mysqli_escape_string($conexao,$_POST["resolucao"]);
 
+date_default_timezone_set('America/Sao_Paulo');
+
 $query = "INSERT INTO registros (responsavel, requisitante, matricula, email, telefone, assunto, resolucao,data,hora) VALUES ('{$responsavel}', '{$requisitante}', '{$matricula}', '{$email}', '{$telefone}', '{$assunto}', '{$resolucao}',SYSDATE(),CURRENT_TIME())";
 
 if($conexao->query($query) === TRUE){
