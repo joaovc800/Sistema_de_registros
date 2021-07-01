7ff88d4a7e1d60eb00b427810b3e19daaf92a22d
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-07-01 19:19:18 -0300

    Update update.php

--- php/update.php
+++ php/update.php
@@ -12,11 +12,14 @@ $n_registro = (int)$_POST["registro"];
 $atualizacao = mysqli_escape_string($conexao,$_POST["res"]);
 $responsavel = mysqli_escape_string($conexao,$_SESSION["nome"]);
 $status = mysqli_escape_string($conexao,$_POST['status']);
+date_default_timezone_set('America/Sao_Paulo');
+$hora = date("H:i");
+$hoje = date('Y-m-d');
 
 $query2 = "UPDATE registros SET `status` = '$status' WHERE id = '$n_registro'";
 $mudar = mysqli_query($conexao,$query2);
 
-$query = "INSERT INTO updates (n_registro,responsavel,atualizacao) VALUES ('{$n_registro}', '{$responsavel}', '{$atualizacao}')";
+$query = "INSERT INTO updates (n_registro,responsavel,atualizacao,data,hora) VALUES ('{$n_registro}', '{$responsavel}', '{$atualizacao}',{$hoje},{$hora})";
 
 if($conexao->query($query) === TRUE){
     $_SESSION["atualizado"] = true;
