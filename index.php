2c3927dc31a3dbaa8cf5828568aa02b7bc4953d4
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-07-03 13:33:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -25,56 +25,90 @@
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/style2.css">
     <title>Log in | UNINOVE</title>
-</head>
-<body style="background-color: #ffffff;"class="text-center mt-5">
-    <div class="container">
-        <img class="mt-5" src="images/uni9.png" width="350px">
-        <form action="php/login.php" method="POST">
-            <div class="container col-md-4">      
-                <div class="mb-3">
-                    <input name="usuario" type="email" class="form-control form-control-lg" placeholder="Usuário">
-                </div>
-            </div>
-            <div class="container col-md-4">
-                <div class="mb-3">
-                    <div id="input3" class="input-group">
-                        <input name="senha" type="password" class="form-control form-control-lg" placeholder="Senha">
-                        <button class="btn btn-outline-secondary" type="button" data-bs-toggle="button"><i class="fas fa-eye"></i></button>
-                    </div>    
-                </div>
-                <button type="submit" class="form-control-lg btn btn-primary w-100">Acessar</button>
-            </div>
-        </form>
-        <div class="container col-md-4">
-        <div class="row">
-        <?php
-            if($_SESSION['login_vazio']){
-        ?>
-        <div class="blr3 card bg-danger mt-3">
-            <p class="text-center text-light">Os campos não podem ficar vazios</p>
-        </div>
-        <?php
-            }
-        unset($_SESSION['login_vazio']);
-        ?>
-        <?php
-            if($_SESSION['login_invalido']){
-        ?>
-        <div class="blr3 card bg-danger mt-3">
-            <p class="text-center text-light">Usuário ou senha inválida, por favor tente novamente</p>
-        </div>
-        <?php
-            }
-        unset($_SESSION['login_invalido']);
-        ?>
-        </div>
-        </div>
-        <div class="mt-3 container-md w-50">
-            <hr>
-        </div>
-        <p class="fw-light">Universidade Nove de Julho</p>
-        <p class="fw-light">All rights reserved</p>
-    </div>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Playball&display=swap&family=Krona+One&display=swap');
+        .bg-light{
+            background-color: #ffffff;
+            display: block;
+        }
+        body{
+            background-color: #e8e8e8;
+        }
+        label{
+            font-family: 'Krona One', sans-serif;
+        }
+        .bg{
+            background-image: linear-gradient(65deg,#3858c6,#38c3fe);
+        }
+    </style>
+    </head>
+<body>
+<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12 text-center mb-5">
+					<h1>Registro SD</h1>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+                        <div class="d-md-flex">
+                            <div class="col-md-6 bg p-lg-5 text-center d-flex align-items-center order-md-last">
+                                <div class="container">
+                                    <img width="80%" src="images/uni9.png">
+                                </div>
+                            </div>
+                        <div class="col-md-6 bg-light p-lg-5">
+                            <div class="d-flex">
+                                <div class="w-100">
+                                    <h3 class="mb-4">Log In</h3>
+                                </div>	
+                            </div>
+                            <form action="php/login.php" method="POST">
+                                <div class="form-group mb-3">
+                                    <label class="label" for="name">Usuário</label>
+                                    <input name="usuario" type="email" class="form-control" placeholder="Usuário" required>
+                                </div>
+                                <label class="label" for="password">Senha</label>
+                                <div id="input3" class="input-group">
+                                    <input name="senha" type="password" class="form-control form-control" placeholder="Senha">
+                                    <button class="btn btn-outline-secondary" type="button" data-bs-toggle="button"><i class="fas fa-eye"></i></button>
+                                </div> 
+                                <div class="form-group mt-3">
+                                    <button type="submit" class="form-control btn btn-outline-dark px-3">Acessar</button>
+                                </div>
+                            </form>
+                            <div class="container col-md-12">
+                                <div class="row">
+                                    <?php
+                                        if($_SESSION['login_vazio']){
+                                    ?>
+                                    <div class="blr3 card bg-danger mt-3">
+                                        <p class="text-center text-light">Os campos não podem ficar vazios</p>
+                                    </div>
+                                    <?php
+                                        }
+                                    unset($_SESSION['login_vazio']);
+                                    ?>
+                                    <?php
+                                        if($_SESSION['login_invalido']){
+                                    ?>
+                                    <div class="blr3 card bg-danger mt-3">
+                                        <p class="text-center text-light">Usuário ou senha inválida, por favor tente novamente</p>
+                                    </div>
+                                    <?php
+                                        }
+                                    unset($_SESSION['login_invalido']);
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+		            </div>
+				</div>
+			</div>
+		</div>
+	</section>
+    
     <script>
         var input3 = document.querySelector('#input3 input');
         var button3 = document.querySelector('#input3 button');
