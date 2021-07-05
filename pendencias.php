ddf29fcdadd912bef0b673a28b9d2fa89d32f638
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-07-05 19:22:10 -0300

    Update pendencias.php

--- pendencias.php
+++ pendencias.php
@@ -88,7 +88,7 @@
             <button type="button" id="sidebarCollapse" class="btn btn-primary">
               <i class="fa fa-bars"></i>
               <span class="sr-only">Toggle Menu</span>
-            </button>>
+            </button>
             <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <i class="fa fa-bars"></i>
             </button>
