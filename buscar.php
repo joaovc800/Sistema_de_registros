57458d317b7fc2e9e127505b43058699cd7d3aa6
Leonardo Ferreira <70867390+LeoACF@users.noreply.github.com>
2021-08-23 09:59:24 -0300

    Update buscar.php
    Foram retiradas as portas "SD" e "OS" dos menus laterais

--- buscar.php
+++ buscar.php
@@ -209,6 +209,6 @@
     <script src="js/jquery.min.js"></script>
     <script src="js/popper.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="js/main.js"></script> 
   </body>
 </html>
