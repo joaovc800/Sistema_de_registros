d395fc032d8f630700e0a2b163a35eeb471937e8
Leonardo Ferreira <70867390+LeoACF@users.noreply.github.com>
2021-08-23 14:12:45 -0300

    Update registros.php

--- registros.php
+++ registros.php
@@ -55,9 +55,6 @@
             <a href="perfil.php"><span class="fas fa-user"></span> Perfil</a>
           </li>
           <li>
-            <a href="http://os.uninove.br/os/" target="_blank"><span class="fas fa-cogs"></span>O.S</a>
-          </li>
-          <li>
             <a href="http://portalsd.uninove.br/" target="_blank"><span class="fas fa-door-open"></span>Portal SD</a>
           </li>
           <li>
