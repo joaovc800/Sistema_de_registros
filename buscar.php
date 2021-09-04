889396347ef1642468c0e028adfc2040c067ba10
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-04 17:28:50 -0300

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -6,7 +6,7 @@
 <!doctype html>
 <html lang="pt-BR">
   <head>
-  	<title>Buscar | UNINOVE</title>
+  	<title>Buscar | Wit Solutions</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
