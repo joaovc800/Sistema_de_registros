6fbb109bea1d3908f0f4bd093f7055485f02a5b0
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-04 16:12:15 -0300

    Update principal.php

--- principal.php
+++ principal.php
@@ -98,6 +98,7 @@
     <main>
     <div class="wrapper d-flex align-items-stretch">
 			<nav id="sidebar" class="active h-100 fixed-top">
+				<a href="principal.php"><img src="images/logo.jpeg"></a>
         <ul class="list-unstyled components mb-5">
           <li class="active">
             <a href="principal.php"><span class="fa fa-home"></span> Home</a>
