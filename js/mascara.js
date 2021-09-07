920d4eee8a79dd15f8ae969e8d5ed2a9f7d3c9d6
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-07 14:58:03 -0300

    ajuste na indentação

--- js/mascara.js
+++ js/mascara.js
@@ -1,5 +1,5 @@
 function mask(o, f) {
-    setTimeout(function() {
+  setTimeout(function () {
     var v = mphone(o.value);
     if (v != o.value) {
       o.value = v;
@@ -7,36 +7,31 @@ function mask(o, f) {
   }, 1);
 }
 
-  function mphone(v) {
-    var r = v.replace(/\D/g, "");
-    r = r.replace(/^0/, "");
-    if (r.length > 10) {
-      r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
-    } else if (r.length > 5) {
-      r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
-    } else if (r.length > 2) {
-      r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
-    } else {
-      r = r.replace(/^(\d*)/, "($1");
-    }
-    return r;
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
   }
+  return r;
+}
 
-  function mascaraData( campo, e ){
-	var kC = (document.all) ? event.keyCode : e.keyCode;
-	var data = campo.value;
-	
-	if( kC!=8 && kC!=46 )
-	{
-		if( data.length==2 )
-		{
-			campo.value = data += '/';
-		}
-		else if( data.length==5 )
-		{
-			campo.value = data += '/';
-		}
-		else
-			campo.value = data;
-	}
+function mascaraData(campo, e) {
+  var kC = (document.all) ? event.keyCode : e.keyCode;
+  var data = campo.value;
+
+  if (kC != 8 && kC != 46) {
+    if (data.length == 2) {
+      campo.value = data += '/';
+    } else if (data.length == 5) {
+      campo.value = data += '/';
+    } else
+      campo.value = data;
+  }
 }
