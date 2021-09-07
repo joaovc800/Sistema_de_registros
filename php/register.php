18a6811d593fbf970402aa0383919b72b222ca19
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-07 15:03:31 -0300

    comentando o código

--- php/register.php
+++ php/register.php
@@ -2,6 +2,7 @@
 session_start();
 include("conexao.php");
 
+// verificar campos vázios
 if(empty($_POST["nome_req"]) || empty($_POST["matricula"]) || empty($_POST["email"]) || empty($_POST["telefone"]) || empty($_POST["assunto"]) || empty($_POST["resolucao"])){
     $_SESSION["campos_vazios"] = true;
     header("Location: ../registrar.php");
@@ -13,6 +14,7 @@ if($_POST["status"] == "Status" || $_POST["status"] == ""){
     exit();
 }
 
+// puxar dados via post com escape para asegurar contra sql injection
 $responsavel = mysqli_escape_string($conexao,$_SESSION["nome"]);
 $requisitante = mysqli_escape_string($conexao,$_POST["nome_req"]);
 $matricula = mysqli_escape_string($conexao,$_POST["matricula"]);
@@ -22,10 +24,12 @@ $assunto = mysqli_escape_string($conexao,$_POST["assunto"]);
 $status = mysqli_escape_string($conexao,$_POST["status"]);
 $resolucao = mysqli_escape_string($conexao,$_POST["resolucao"]);
 
+// formatação de data
 date_default_timezone_set('America/Sao_Paulo');
 $hora = date("H:i");
 $hoje = date('Y-m-d');
 
+// query de inserção do registro
 $query = "INSERT INTO registros (responsavel, requisitante, matricula, email, telefone, assunto,status,resolucao,data,hora) VALUES ('{$responsavel}', '{$requisitante}', '{$matricula}', '{$email}', '{$telefone}', '{$assunto}','{$status}','{$resolucao}','{$hoje}','{$hora}')";
 
 if($conexao->query($query) === TRUE){
