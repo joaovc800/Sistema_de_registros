422dd99e11a1adf9b2f352c6e00718f81cead4f4
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-07 15:01:33 -0300

    comentando o código

--- php/muda_senha.php
+++ php/muda_senha.php
@@ -1,22 +1,25 @@
 <?php
-session_start();
-include('conexao.php');
+session_start(); //startando a sessão
+include('conexao.php'); // incluindo o arquivo de conexão
 
+// condição para troca de senha
 if(empty($_POST['senha1']) || empty($_POST['senha2'])){
 	$_SESSION['senha_vazia'] = true;
 	header('Location: ../perfil.php');
     exit();
 }
 
-$senha1 = mysqli_real_escape_string($conexao,md5($_POST['senha1']));
-$senha2 = mysqli_real_escape_string($conexao,md5($_POST['senha2']));
+$senha1 = mysqli_real_escape_string($conexao,md5($_POST['senha1'])); // puxar dados via post com espape para proteger comtra mysql injection
+$senha2 = mysqli_real_escape_string($conexao,md5($_POST['senha2'])); // puxar dados via post com espape para proteger comtra mysql injection
 
+// validar se as senhas não correspondem
 if($senha1 != $senha2){
 	$_SESSION['senha_nao_corresponde'] = true;
 	header('Location: ../perfil.php');
 	return;
 }
 
+// query para alteração de senha
 $query = "UPDATE usuario SET senha ='{$senha2}' WHERE email = '{$_SESSION['usuario']}'";
 
 if($conexao->query($query) === TRUE){
