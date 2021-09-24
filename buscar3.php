2976f59e244ec56640abdbdd93cfafbb8ae6fd9a
João <joaoviniciusdacosta800@gmail.com>
2021-09-24 18:38:39 -0300

    Incluimos os menus no fonte especifico para chamar atraves de um inluide

--- buscar3.php
+++ buscar3.php
@@ -40,52 +40,9 @@ include('php/conexao.php');
     <!-- Page Content  -->
     <div id="content" class="container w-75 col-md-8 p-2 p-md-5">
 
-      <nav class="sli navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-
-          <button type="button" id="sidebarCollapse" class="btn btn-primary">
-            <i class="fa fa-bars"></i>
-            <span class="sr-only">Toggle Menu</span>
-          </button>
-          <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fa fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="principal.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="registrar.php">Novo registro</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="registros.php">Registros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="pendencias.php">Pendentes</a>
-              </li>
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a class="nav-link" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Buscar
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-                    <li><a class="dropdown-item text-center text-dark" href="buscar2.php">Por Data</a></li>
-                    <li><a class="dropdown-item text-center text-dark" href="buscar.php">Por Matricula ou RA</a></li>
-                    <li><a class="dropdown-item text-center text-dark" href="buscar3.php">Por Assunto</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <li class="nav-item">
-                <a class="nav-link" href="perfil.php">Perfil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="php/logout.php">Sair</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <!-- Menu superior -->
+    <?php include "php/menu_superior.php"?>
+    
       <form action="buscar3.php" method="POST">
         <div class="row">
           <div class="input-group mb-3 col-sm-12">
--- registros.php
+++ registros.php
@@ -33,98 +33,13 @@ include("php/verifica_login.php");
 <body>
 
   <div class="wrapper d-flex align-items-stretch">
-    <nav id="sidebar" class="active h-100 fixed-top">
-      <ul class="list-unstyled components mb-5">
-        <li>
-          <img class="w-100" src="images/logo.jpeg">
-        </li>
-        <li class="active">
-          <a href="principal.php"><span class="fa fa-home"></span> Home</a>
-        </li>
-        <li class="">
-          <a href="registrar.php"><span class="fa fa-pen"></span> Novo</a>
-        </li>
-        <li>
-          <a href="registros.php"><span class="fa fa-folder-open"></span>Registros</a>
-        </li>
-        <li>
-          <a href="pendencias.php"><span class="fas fa-clock"></span>Pendentes</a>
-        </li>
-        <li class="nav-item dropend">
-          <a class="nav-link" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><span class="fas fa-search"></span>Buscar</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item text-light text-center" href="buscar2.php">Por Data</a></li>
-            <li><a class="dropdown-item text-light text-center" href="buscar.php">Por Matricula ou RA</a></li>
-            <li><a class="dropdown-item text-light text-center" href="buscar3.php">Por Assunto</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="perfil.php"><span class="fas fa-user"></span> Perfil</a>
-        </li>
-        <li>
-          <a href="php/logout.php"><span class="fas fa-sign-out-alt"></span>Sair</a>
-        </li>
-      </ul>
-
-      <div class="footer fixed-bottom">
-        <p class="copy mx-2">
-          WIT solutions | Dev © <i class="icon-heart" aria-hidden="true"></i>
-          <script>
-            document.write(new Date().getFullYear());
-          </script>
-        </p>
-      </div>
-    </nav>
+    <?php include "php/menu.php" ?>
 
     <!-- Page Content  -->
     <div id="content" class="container w-75 col-md-8 p-2 p-md-5">
 
-      <nav class="sli navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-
-          <button type="button" id="sidebarCollapse" class="btn btn-primary">
-            <i class="fa fa-bars"></i>
-            <span class="sr-only">Toggle Menu</span>
-          </button>
-          <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <i class="fa fa-bars"></i>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="nav navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link" href="principal.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="registrar.php">Novo registro</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="registros.php">Registros</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="pendencias.php">Pendentes</a>
-              </li>
-              <ul class="navbar-nav">
-                <li class="nav-item dropdown">
-                  <a class="nav-link" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    Buscar
-                  </a>
-                  <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
-                    <li><a class="dropdown-item text-center text-dark" href="buscar2.php">Por Data</a></li>
-                    <li><a class="dropdown-item text-center text-dark" href="buscar.php">Por Matricula ou RA</a></li>
-                    <li><a class="dropdown-item text-center text-dark" href="buscar3.php">Por Assunto</a></li>
-                  </ul>
-                </li>
-              </ul>
-              <li class="nav-item">
-                <a class="nav-link" href="perfil.php">Perfil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="php/logout.php">Sair</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <!-- Menu superior -->
+    <?php include "php/menu_superior.php"?>
       <div class="container">
         <h3 class="text-center">Registros de hoje</h3>
         <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
