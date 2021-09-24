0db302f0ba6e4ec8b0e89ea431fee43591eecefd
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-09-24 18:21:29 -0300

    Update buscar.php

--- buscar.php
+++ buscar.php
@@ -34,50 +34,9 @@ include('php/conexao.php');
 <body>
 
   <div class="wrapper d-flex align-items-stretch">
+	<!-- menu sidebar -->  
     <?php include "php/menu.php" ?>
-    <!--<nav id="sidebar" class="active h-100 fixed-top">
-      <ul class="list-unstyled components mb-5">
-        <li>
-          <img class="w-100" src="images/logo.jpeg">
-        </li>
-        <li class="active">
-          <a href="principal.php"><span class="fa fa-home"></span> Home</a>
-        </li>
-        <li class="">
-          <a href="registrar.php"><span class="fa fa-pen"></span> Novo</a>
-        </li>
-        <li>
-          <a href="registros.php"><span class="fa fa-folder-open"></span>Registros</a>
-        </li>
-        <li>
-          <a href="pendencias.php"><span class="fas fa-clock"></span>Pendentes</a>
-        </li>
-        <li class="nav-item dropend">
-          <a class="nav-link" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><span class="fas fa-search"></span>Buscar</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item text-light text-center" href="buscar2.php">Por Data</a></li>
-            <li><a class="dropdown-item text-light text-center" href="buscar.php">Por Matricula ou RA</a></li>
-            <li><a class="dropdown-item text-light text-center" href="buscar3.php">Por Assunto</a></li>
-          </ul>
-        </li>
-        <li>
-          <a href="perfil.php"><span class="fas fa-user"></span> Perfil</a>
-        </li>
-        <li>
-          <a href="php/logout.php"><span class="fas fa-sign-out-alt"></span>Sair</a>
-        </li>
-      </ul>
-
-      <div class="footer fixed-bottom">
-        <p class="copy mx-2">
-          WIT solutions | Dev © <i class="icon-heart" aria-hidden="true"></i>
-          <script>
-            document.write(new Date().getFullYear());
-          </script>
-        </p>
-      </div>
-    </nav> -->
-
+	  
     <!-- Page Content  -->
     <div id="content" class="container w-75 col-md-8 p-2 p-md-5">
 
